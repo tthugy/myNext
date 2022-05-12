3552c6e2076da53ae6e1cd5cad84edb73c03b088
tthugy <supermanace7@gmail.com>
2022-05-12 23:21:26 +0900

    Pre-Rendering, Routing

--- nextjs/pages/about.js
+++ nextjs/pages/about.js
@@ -1,11 +1,12 @@
+
 import NavBar from "../component/NavBar";
 
-export default function Home(){
+export default function About(){
 
     return (
         <div>
             <NavBar />
-            <h1>Hello</h1>
+            <h1>about us</h1>
         </div>
     );
 }
