69809f14b8d0fe23d807db611f1ba7b9b3bb976c
tthugy <supermanace7@gmail.com>
2022-05-16 22:01:53 +0900

    fetch data

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -5,7 +5,6 @@ export default function App({ Component, pageProps }){
     return (
         <Layout>
             <Component {...pageProps} />
-            <span>hello</span>
             <style jsx global>{`
             nav {
                 background: #fff;
