2518c4343d268692eb3940bf99fdedc7b5df615a
tthugy <supermanace7@gmail.com>
2022-05-17 21:40:18 +0900

    update

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -18,18 +18,21 @@ export default function Home(){
     }, []);
 
     return (
-        <div>
+        <div className="cont">
             <Seo title="Home" />
             {!movies && <h4>Loading...</h4>}
             {movies?.map((movie) => (
                 <div className="movie" key={movie.id}>
-                    <div className="cont">
-                        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-                        <h4>{movie.original_title}</h4>
-                    </div>
+                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                    <h4>{movie.original_title}</h4>
                 </div>
             ))}
             <style jsx>{`
+            .cont {
+                max-width: 1004px;
+                margin: 0 auto;
+                margin-top: 40px;
+            }
             .container {
             display: grid;
             grid-template-columns: 1fr 1fr;
@@ -37,36 +40,23 @@ export default function Home(){
             gap: 20px;
             }
             .movie {
-                width: 50%;
+                width: 40%;
+                margin: 0 5%;
                 display: inline-block;
             }
-            .cont {
-                width:80%;
-                margin: 0 auto;
-                position: relative;
-                min-height: 820px;
-            }
             .movie img {
-            position: absolute;
-            top: 3%;
-            left: 50%;
-            transform: translate(-50%, 0);
             max-width: 100%;
             border-radius: 12px;
             border: 1px solid #ccc;
-            cursor: pointer;
             transition: transform 0.2s ease-in-out;
             box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
             }
             .movie:hover img {
+            transform: scale(1.05) translateY(-10px);
             }
             .movie h4 {
             font-size: 21px;
             text-align: center;
-            position: absolute;
-            bottom: 5%;
-            left: 50%;
-            transform: translate(-50%, 0);
             }
             `}</style>
         </div>
